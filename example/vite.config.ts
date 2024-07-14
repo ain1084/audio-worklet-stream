31d75f1234dd67e7b95efe1ea9abc2d04ac1a49d
Seiji Ainoguchi <14823909+ain1084@users.noreply.github.com>
2024-07-14 02:09:09 +0000

    Added optimizeDeps.exclude

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './',
+  optimizeDeps: {
+    exclude: ['@ain1084/audio-worklet-stream'],
+  },
   plugins: [
     {
       name: 'configure-response-headers',
